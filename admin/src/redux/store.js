60e4c33e2e982031da0716814f181685ed86efe6
Shankar4919 <itsme.shankar8@gmail.com>
2022-06-08 11:03:36 +0545

    user list

--- admin/src/redux/store.js
+++ admin/src/redux/store.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch as useAppDispatch, useSelector as useAppSelector } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { rootPersistConfig, rootReducer } from './rootReducer';
 
-const store = () => {
-  return (
-    <div>store</div>
-  )
-}
+// ----------------------------------------------------------------------
 
-export default store
+const store = configureStore({
+  reducer: persistReducer(rootPersistConfig, rootReducer),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+      immutableCheck: false,
+    }),
+});
+
+const persistor = persistStore(store);
+
+const { dispatch } = store;
+
+const useSelector = useAppSelector;
+
+const useDispatch = () => useAppDispatch();
+
+export { store, persistor, dispatch, useSelector, useDispatch };
